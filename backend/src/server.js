45b9fc3d49a74f5af0afac7eefb3a82e38c672be
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-07-09 15:03:59 +0100

    New commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 app.listen(4000, (req, res) => {
     console.log('RUNNING');
