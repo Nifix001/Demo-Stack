981162f6fa2ec3a195bab14736910492a9e4c474
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-07-26 09:08:35 +0100

    New commit

--- frontend/demo-stack/src/pages/Register.jsx
+++ frontend/demo-stack/src/pages/Register.jsx
@@ -13,6 +13,14 @@ const Register = () => {
     password:"",
   })
 
+  function handleChange(event) {
+    const {name, value} = event.target
+    setUserData(prevData => ({
+      ...prevData,
+      [name]: value 
+    }))
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
     try{
@@ -28,28 +36,29 @@ const Register = () => {
     }
   }
   
+
   return (
     <React.Fragment>
       <fieldset>
         <Form className="register" onSubmit={handleSubmit} method="post">
           <label htmlFor="id">Id</label>
-          <input type="number" name="id" value={userData.id} required />
+          <input type="number" name="id" value={userData.id} onChange={handleChange} required />
           <label htmlFor="first_name">First Name</label>
-          <input type="text"placeholder="enter your first name" name="first_name" value={userData.first_name} required />
+          <input type="text" placeholder="enter your first name" name="first_name" value={userData.first_name} onChange={handleChange} required />
           <label htmlFor="last_name">Last Name</label>
-          <input type="text" placeholder="enter your last name" name="last_name" value={userData.last_name} required />
+          <input type="text" placeholder="enter your last name" name="last_name" value={userData.last_name} onChange={handleChange} required />
           <label htmlFor="email">Email</label>
-          <input type="email" placeholder="enter your email" name="email" value={userData.email} required />
+          <input type="email" placeholder="enter your email" name="email" value={userData.email} onChange={handleChange} required />
           <label htmlFor="ip_address">IP Address</label>
-          <input type="text" placeholder="enter your IP address" name="ip_address" value={userData.ip_address} required />
+          <input type="text" placeholder="enter your IP address" name="ip_address" value={userData.ip_address} onChange={handleChange} required />
           <label htmlFor="gender">Gender</label>
-          <select name="gender" value={userData.gender} required>
+          <select name="gender" value={userData.gender} onChange={handleChange} required>
             <option value="" disabled>Select your gender</option>
             <option value="">Male</option>
             <option value="">Female</option>
           </select>
           <label htmlFor="password">Password</label>
-          <input type="password" placeholder="enter your pasword" name="pasword" value={userData.password} required />
+          <input type="password" placeholder="enter your pasword" name="pasword" value={userData.password} onChange={handleChange} required />
           <button type="submit">Register</button>
         </Form>
         <p>Registered already <Link to='/login' >Login</Link></p>
