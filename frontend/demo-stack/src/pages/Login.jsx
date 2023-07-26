b52e642b3118e61a1737c30aa1761421724233e1
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-07-26 08:21:42 +0100

    commit

--- frontend/demo-stack/src/pages/Login.jsx
+++ frontend/demo-stack/src/pages/Login.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import { Form, Link } from "react-router-dom"
 
 const Login = () => {
+
+  const [user, setUser] = useState({
+    email:"",
+    password:"",
+  })
+
   return (
     <React.Fragment>
       <fieldset>
-      <Form className="login">
-        <label>Email</label>
-        <input type="email" required />
-        <label>Password</label>
-        <input type="password" required />
+      <Form className="login" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="email" placeholder="Enter e-mail" name="email" value={user.email} required />
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="enter your password" name="password" value={user.password} required />
         <button>Login</button>
       </Form>
       <p>Not registered yet? <Link to='/register'>Register here</Link></p>
